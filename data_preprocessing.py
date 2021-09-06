22de939a5f21b859164d3c52d62e8795001a72cc
EUNJEEEE HA <87507439+EUNJEEHA@users.noreply.github.com>
2021-09-06 20:59:10 +0900

    preprocessing_chart
    1. 6,7,8월 데이터만 추출
    2. "장르"열 데이터값 수정

--- data_preprocessing.py
+++ data_preprocessing.py
@@ -15,7 +15,7 @@ for name in excel_names:
     pd_data = pd.read_excel(name)
     appended_data = appended_data.append(pd_data)
 
-appended_data.to_excel("C:\\Users\\USER\\Desktop\\DS\\project1\\data\\merge_month.xlsx", index=False)
+# appended_data.to_excel("C:\\Users\\USER\\Desktop\\DS\\project1\\data\\merge_month.xlsx", index=False)
 
 
 # "appended_data" : 원본 객체
@@ -27,7 +27,7 @@ appended_data.to_excel("C:\\Users\\USER\\Desktop\\DS\\project1\\data\\merge_mont
 summer_data = appended_data[(appended_data["월"]==6) | (appended_data["월"]==7) |
                                          (appended_data["월"]==8)]
 
-summer_data.to_excel("C:\\Users\\USER\\Desktop\\DS\\project1\\data\\summer_data.xlsx", index=False)
+# summer_data.to_excel("C:\\Users\\USER\\Desktop\\DS\\project1\\data\\summer_data.xlsx", index=False)
 summer_data = pd.DataFrame(summer_data,columns=["년도","월","순위","타이틀","가수","장르","해당년도"])
 
 
@@ -36,8 +36,15 @@ summer_data["장르"] = summer_data["장르"].replace(["OST / 드라마", "OST /
 # print(summer_data.head())
 
 # "장르"열에서 "가요" 전처리
-summer_data["장르"] = summer_data["장르"].replace(["가요 / R&B/소울","가요 / 댄스","가요 / 인디","가요 / 발라드","가요 / 랩/힙합","가요 / 락","가요 / 블루스/포크","가요 / 일렉트로니카",],
-                                       ["R&B/소울","댄스","인디","발라드","랩/힙합","락","블루스/포크","일렉트로니카"])
+summer_data["장르"] = summer_data["장르"].replace(["가요 / R&B/소울","가요 / 댄스","가요 / 인디","가요 / 발라드",
+                                               "가요 / 랩/힙합","가요 / 락","가요 / 블루스/포크","가요 / 일렉트로니카",
+                                               "POP / 락","POP / 일렉트로니카"," 인디"," 댄스"," 랩/힙합"," 발라드",
+                                               " 드라마"," 팝"," 일렉트로니카"," R&B/소울"," 락","OST / 해외영화",
+                                               "가요 / 트로트","POP / R&B/소울","가요 / 전체"," 전체","POP / 팝","팝",
+                                               "드라마","재즈 / 정통"],
+                                       ["R&B/소울","댄스","인디","발라드","랩/힙합","락","블루스/포크","일렉트로니카",
+                                        "POP","POP","인디","댄스","랩/힙합","발라드","OST","POP","일렉트로니카","R&B/소울",
+                                        "락","OST","트로트","POP","전체","전체","POP","POP","OST","재즈"])
 
 # "해당년도" 열 삭제
 prepro_data = summer_data.drop("해당년도",axis=1)
