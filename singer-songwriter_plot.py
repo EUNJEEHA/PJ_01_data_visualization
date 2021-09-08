819d95775586aa2d6bc502df59fe6c882bee21a9
EUNJEEEE HA <87507439+EUNJEEHA@users.noreply.github.com>
2021-09-08 09:47:24 +0900

    singersongwriter_barplot_code

--- singer-songwriter_plot.py
+++ singer-songwriter_plot.py
@@ -29,6 +29,11 @@ df17 = df17[is_lyricist | is_songwriter]
 # print(df17.head())
 # print(df17["가수"].count())
 
+# 중복 제거
+df17.drop_duplicates(['타이틀'], inplace=True, ignore_index=True)
+print(df17.head())
+print(df17["가수"].count())
+
 # [연도 : 곡 수] 형태의 데이터프레임으로 만들기
 data_table = {"연도":"2017", "곡 수":df17["가수"].count()}
 # print(data_table)
@@ -45,6 +50,11 @@ is_songwriter = df18['가수'] == df18['작곡가']
 df18 = df18[is_lyricist | is_songwriter]
 # print(df18.head())
 # print(df18["가수"].count())
+
+# 중복 제거
+df18.drop_duplicates(['타이틀'], inplace=True, ignore_index=True)
+print(df18.head())
+print(df18["가수"].count())
 
 # [연도 : 곡 수] 형태의 데이터프레임으로 만들기
 data_table = {"연도":"2018", "곡 수":df18["가수"].count()}
@@ -63,6 +73,11 @@ df19 = df19[is_lyricist | is_songwriter]
 # print(df19.head())
 # print(df19["가수"].count())
 
+# 중복 제거
+df19.drop_duplicates(['타이틀'], inplace=True, ignore_index=True)
+print(df19.head())
+print(df19["가수"].count())
+
 # [연도 : 곡 수] 형태의 데이터프레임으로 만들기
 data_table = {"연도":"2019", "곡 수":df19["가수"].count()}
 # print(data_table)
@@ -80,6 +95,11 @@ df20 = df20[is_lyricist | is_songwriter]
 # print(df20.head())
 # print(df20["가수"].count())
 
+# 중복 제거
+df20.drop_duplicates(['타이틀'], inplace=True, ignore_index=True)
+print(df20.head())
+print(df20["가수"].count())
+
 # [연도 : 곡 수] 형태의 데이터프레임으로 만들기
 data_table = {"연도":"2020", "곡 수":df20["가수"].count()}
 # print(data_table)
@@ -94,8 +114,11 @@ print(type(df_2020))
 is_lyricist = df21['가수'] == df21['작사가']
 is_songwriter = df21['가수'] == df21['작곡가']
 df21 = df21[is_lyricist | is_songwriter]
-# print(df21.head())
-# print(df21["가수"].count())
+
+# 중복 제거
+df21.drop_duplicates(['타이틀'], inplace=True, ignore_index=True)
+print(df21.head())
+print(df21["가수"].count())
 
 # [연도 : 곡 수] 형태의 데이터프레임으로 만들기
 data_table = {"연도":"2021", "곡 수":df21["가수"].count()}
@@ -107,38 +130,26 @@ print(type(df_2021))
 
 
 # 하나의 데이터프레임으로 merge (2021년 제외)
-df_merge = df_2017.append([df_2018,df_2019,df_2020], ignore_index=True)
-print(df_merge)
+df_merge_1 = df_2017.append([df_2018,df_2019,df_2020], ignore_index=True)
+print(df_merge_1)
 
-# 막대 그래프
-df_bar = df_merge.plot.bar(x="연도")
+# 막대 그래프(2017~2020)
+df_bar = df_merge_1.plot.bar(x="연도")
 df_bar.set_xlabel("연도")
 df_bar.set_ylabel("곡 수")
 df_bar.set_title("연도별 싱어송라이터의 곡 수")
-plt.ylim(100,300)
-plt.savefig("C:\\Users\\USER\\Desktop\\DS\\project1\\data\\singersongwriter_bar.png",dpi=200)
+plt.ylim(20,70)
+plt.savefig("C:\\Users\\USER\\Desktop\\DS\\project1\\data\\singersongwriter_bar_1.png",dpi=200)
 plt.show()
 
 
+# 하나의 데이터프레임으로 merge (2021년 포함)
+df_merge_2 = df_2017.append([df_2018,df_2019,df_2020,df_2021], ignore_index=True)
 
-
-# df_2017 = df.loc[(df_2017.가수 == df_2017.작사가) | (df_2017.가수 == df_2017.작곡가)]
-# df_6.to_excel("C:\\Users\\USER\\Desktop\\DS\\project1\\data\\6_data.xlsx", index=False)
-# print(df_2017.head())
-
-# GenreCount_Summer = pd.DataFrame([df_6['장르'].value_counts(), df_7['장르'].value_counts(),
-#                            df_8['장르'].value_counts()],
-#                            index=['6월', '7월', '8월'])
-# GenreCount_Summer = GenreCount_Summer.T
-# print(GenreCount_Summer.head())
-
-# print(type(df_list))  # 데이터프레임
-
-
-
-
-
-
-
-
-
+df_bar = df_merge_2.plot.bar(x="연도")
+df_bar.set_xlabel("연도")
+df_bar.set_ylabel("곡 수")
+df_bar.set_title("연도별 싱어송라이터의 곡 수")
+plt.ylim(20,70)
+plt.savefig("C:\\Users\\USER\\Desktop\\DS\\project1\\data\\singersongwriter_bar_2.png",dpi=200)
+plt.show()
